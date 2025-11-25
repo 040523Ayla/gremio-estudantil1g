2da74479adab0272c1b2cb2338803dc143abc4c1
040523Ayla <raissa.gustavo.nascimento@escola.pr.gov.br>
2025-11-25 20:24:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function tocaSom(idElementoaudio){
+const elemento=Document.QuerySelector(idElementoaudio)
+if(elemento && elemento.localName=== 'audio'){
+      elemento.play ();
+}else{
+     console.log'Elemento não encontrado invalido';
+}
+}    
+  const listaDeTeclas= Document.QuerySelector
+
+
